2788dc8ef93ee222c832b8b2c38a891f802d5956
balajirv <ramini.venkatesh@gmail.com>
2022-11-16 17:20:49 +0530

    first commit

---
+++ src/main/java/com/leetcode/ContainsDuplicate_II/ContainsDuplicateIiApplication.java
@@ -0,0 +1,13 @@
+package com.leetcode.ContainsDuplicate_II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainsDuplicateIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainsDuplicateIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leetcode/ContainsDuplicate_II/ContainsDuplicateIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.ContainsDuplicate_II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainsDuplicateIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
